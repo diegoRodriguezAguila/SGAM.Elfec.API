8eeec5d31d899519bc24d06213ed940ba9896fd0
diegoRodriguezAguila <diroag@gmail.com>
2015-09-02 11:18:39 -0400

    Agregado modelo de usuario y sus tipos de validaciones

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    username {FFaker::Internet.user_name}
+    password FFaker::Internet.password(0, 10)
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:username) }
+  it { should respond_to(:password) }
+  it { should be_valid }
 end
