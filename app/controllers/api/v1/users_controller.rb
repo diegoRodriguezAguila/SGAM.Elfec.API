4df6d07d0b7eef58d7747a38f91b74e1679ba461
diegoRodriguezAguila <diroag@gmail.com>
2015-10-07 11:40:54 -0400

    Ahora se tiene la opción de asignar uno o mas dispositivos a un usuario, asi como quitar dichas asignaciones

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,17 +20,32 @@ class Api::V1::UsersController < ApplicationController
 
   def assign_devices
     user = User.find_by(username: params[:user_id])
-    devices_to_add = Device.where(imei: user_device_imeis_params, status: Device.statuses[:authorized])
-    if (user.nil? || devices_to_add.empty?)
-      render json: {errors: I18n.t(:'api.errors.user.invalid_device_assignations', :cascade => true)}, status: :bad_request
+    if user.nil?
+      head :not_found
     else
-      user.devices << devices_to_add
-      head :no_content
+      devices_to_add = Device.where(imei: user_device_imeis_params, status: Device.statuses[:authorized]) - user.devices
+      if devices_to_add.empty? || devices_to_add.size<user_device_imeis_params.size
+        render json: {errors: I18n.t(:'api.errors.user.device_assignations', :cascade => true)}, status: :bad_request
+      else
+        user.devices << devices_to_add
+        head :no_content
+      end
     end
   end
 
   def remove_device_assignations
-
+    user = User.find_by(username: params[:user_id])
+    if user.nil?
+      head :not_found
+    else
+      devices_to_del = Device.where(imei: user_device_imeis_params, status: Device.statuses[:authorized])
+      if devices_to_del.empty? || !(devices_to_del - user.devices).empty? || devices_to_del.size<user_device_imeis_params.size
+        render json: {errors: I18n.t(:'api.errors.user.delete_device_assignations', :cascade => true)}, status: :bad_request
+      else
+        user.devices = user.devices - devices_to_del
+        head :no_content
+      end
+    end
   end
 
   private
@@ -39,8 +54,9 @@ class Api::V1::UsersController < ApplicationController
     params.require(:user).permit(:username)
   end
 
-  # Order params should be ?order=name,status:desc
+# Order params should be ?order=name,status:desc
   def user_device_imeis_params
     params[:imeis].gsub(/\s+/, '').split(',')
   end
+
 end
