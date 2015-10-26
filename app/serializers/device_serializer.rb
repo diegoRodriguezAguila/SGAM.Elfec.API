135d3a4aa0026267517be52170246ad38c09d5ed
diegoRodriguezAguila <diroag@gmail.com>
2015-10-26 14:51:28 -0400

    Serializador de dispositivos modificado para que provea de una imagen default si es que no existe una imagen para el modelo del dispositivo

--- app/serializers/device_serializer.rb
+++ app/serializers/device_serializer.rb
@@ -1,7 +1,16 @@
 class DeviceSerializer < ModelWithStatusSerializer
+  include ActionView::Helpers::AssetUrlHelper
   attributes  :name, :imei, :serial, :wifi_mac_address, :bluetooth_mac_address, :platform, :os_version, :baseband_version,
               :brand, :model, :phone_number, :id_cisco_asa, :screen_size, :screen_resolution, :camera, :sd_memory_card,
               :gmail_account, :comments, :status
   self.root = false
+
+  def attributes
+    data = super
+    model_png = "#{model}.png"
+    model_png = 'default_device.png' if Rails.application.assets.find_asset("devices/#{model}.png").nil?
+    data[:icon_url] = "assets/devices/#{model_png}"
+    data
+  end
 
 end
