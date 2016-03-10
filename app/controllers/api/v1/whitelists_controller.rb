a93a691cd8022a1c0d7a2e62e65941c682b25741
diegoRodriguezAguila <diroag@gmail.com>
2016-03-10 11:27:27 -0400

    Se implementó controller para eliminar apps a las whitelist, Some refactorings

--- app/controllers/api/v1/whitelists_controller.rb
+++ app/controllers/api/v1/whitelists_controller.rb
@@ -6,12 +6,9 @@ class Api::V1::WhitelistsController < ApplicationController
   def show
     # searches by hashed id
     whitelist = Whitelist.find_by(id: HASHIDS.decode(params[:id]))
-    if whitelist.nil?
-      head :not_found
-    else
-      raise Exceptions::SecurityTransgression unless whitelist.viewable_by? current_user
-      render json: whitelist, include: request_includes, host: request.host_with_port, status: :ok
-    end
+    return head :not_found if whitelist.nil?
+    raise Exceptions::SecurityTransgression unless whitelist.viewable_by? current_user
+    render json: whitelist, include: request_includes, host: request.host_with_port, status: :ok
   end
 
   #GET whitelists
@@ -36,15 +33,12 @@ class Api::V1::WhitelistsController < ApplicationController
   def update
     # searches by hashed id
     whitelist = Whitelist.find_by(id: HASHIDS.decode(params[:id]))
-    if whitelist.nil?
-      head :not_found
+    return head :not_found if whitelist.nil?
+    raise Exceptions::SecurityTransgression unless whitelist.updatable_by? current_user
+    if whitelist.update(whitelist_params)
+      render json: whitelist, status: :ok, location: [:api, whitelist]
     else
-      raise Exceptions::SecurityTransgression unless whitelist.updatable_by? current_user
-      if whitelist.update(whitelist_params)
-        render json: whitelist, status: :ok, location: [:api, whitelist]
-      else
-        render json: {errors: whitelist.errors.full_messages[0]}, status: :unprocessable_entity
-      end
+      render json: {errors: whitelist.errors.full_messages[0]}, status: :unprocessable_entity
     end
   end
 
@@ -60,37 +54,27 @@ class Api::V1::WhitelistsController < ApplicationController
 
   def add_permitted_apps
     whitelist = Whitelist.find_by(id: HASHIDS.decode(params[:whitelist_id]))
-    if whitelist.nil?
-      head :not_found
-    else
-      apps_to_permit = []
-      whitelist_app_packages_params.each do |package|
-        app = WhitelistApp.find_or_create_by(package: package, whitelist_id: whitelist.id)
-        unless app.errors.empty?
-          return render json: {errors: app.errors.full_messages[0]}, status: :unprocessable_entity
-        end
-        apps_to_permit << app
+    return head :not_found if whitelist.nil?
+    raise Exceptions::SecurityTransgression unless whitelist.updatable_by? current_user
+    apps_to_permit = []
+    whitelist_app_packages_params.each do |package|
+      app = WhitelistApp.find_or_create_by(package: package, whitelist_id: whitelist.id)
+      unless app.errors.empty?
+        return render json: {errors: app.errors.full_messages[0]}, status: :unprocessable_entity
       end
-      apps_to_permit = apps_to_permit - whitelist.permitted_apps
-      whitelist.permitted_apps << apps_to_permit
-      head :no_content
+      apps_to_permit << app
     end
+    apps_to_permit = apps_to_permit - whitelist.permitted_apps
+    whitelist.permitted_apps << apps_to_permit
+    head :no_content
   end
 
   def remove_permitted_apps
     whitelist = Whitelist.find_by(id: HASHIDS.decode(params[:whitelist_id]))
-    if whitelist.nil?
-      head :not_found
-    else
-      users_to_del = User.where(username: user_group_usernames_params)
-      if users_to_del.empty? || !(users_to_del - whitelist.members).empty? ||
-          users_to_del.size<user_group_usernames_params.size
-        render json: {errors: I18n.t(:'api.errors.user_group.delete_user_memberships', cascade: true)}, status: :bad_request
-      else
-        whitelist.members = whitelist.members - users_to_del
-        head :no_content
-      end
-    end
+    return head :not_found if whitelist.nil?
+    raise Exceptions::SecurityTransgression unless whitelist.updatable_by? current_user
+    whitelist.permitted_apps.where(package: whitelist_app_packages_params).destroy_all
+    head :no_content
   end
 
 
